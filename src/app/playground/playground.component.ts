7678be0d97093c1cc1545e62cd263f30e8373a90
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-08 21:22:56 +0530

    done

--- src/app/playground/playground.component.ts
+++ src/app/playground/playground.component.ts
@@ -7,8 +7,19 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 })
 export class PlaygroundComponent implements OnInit {
   @Output() customEvent = new EventEmitter();
+  counter = 0;
 
   constructor() {}
 
   ngOnInit() {}
+
+  clickHandler() {
+    this.counter++;
+
+    this.customEvent.emit({
+      on: new Date(),
+      message: 'Custom event emitted from click handler',
+      clickCount: this.counter
+    });
+  }
 }
