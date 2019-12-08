a17552c3e24c6075761c24a6b258ec24873dd0cf
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-08 21:01:53 +0530

    done

--- src/app/playground/playground.component.ts
+++ src/app/playground/playground.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-playground',
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./playground.component.css']
 })
 export class PlaygroundComponent implements OnInit {
+  //  Custom event-binding is how a child component can communicate with its parent
 
-  constructor() { }
+  //  There are three steps to this communication
 
-  ngOnInit() {
-  }
+  //  1.  Publish and expose the custom event in the child
+  //  2.  Parent hooks to this custom event by providing the handler
+  //  3.  At some juncture, the child emits the custom event
 
+  //  EventEmitter should come from @angular/core package
+  //  @Output() decorator exposes this custom event to the parent
+  //  Parent hooks (by providing the handler) to this event
+  //  in the same way a standard DOM event is hooked
+  //  It is possible to alias this customEvent field as @Output('alias')
+  //  This practice is discouraged and warned
+
+  //  Please note, this is step 1
+  @Output() customEvent = new EventEmitter();
+
+  constructor() {}
+
+  ngOnInit() {}
 }
