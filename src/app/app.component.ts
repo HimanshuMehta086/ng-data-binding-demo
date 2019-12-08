a3c26f7ac12966073d6779142c2b20de4fd11bf2
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-08 20:32:30 +0530

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-data-binding-demo';
+
+  constructor() {
+    setTimeout(() => {
+      this.title = 'Changed title at the parent';
+    }, 2000);
+  }
+
+  foo() {
+    //  [title] depends on foo(), which depends on this.title
+    //  Angular fires the change detection cycle
+    //  when timer is triggered in the constructor.
+    //  This change eventually permeates to the child.
+    return `foo() in AppComponent provides: ${this.title}`;
+  }
 }
