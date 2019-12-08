e7e31d657f17334b31cad56d556a55eb17996ebf
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-08 19:30:28 +0530

    done

--- src/app/currency-clone.pipe.ts
+++ src/app/currency-clone.pipe.ts
@@ -4,18 +4,17 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'currencyClone'
 })
 export class CurrencyClonePipe implements PipeTransform {
-  //  Pipe is a declarable.
-  //  Pipe is also a class which implements the PipeTransform interface
-  //  name property in the above decorator is how we use it in the template
-
-  //  Type-annotating for this method is always a good idea
-  transform(value: any, ...args: any[]): any {
-    //  In the example {{ x | pipe:'a':'b':'c' }} x is the value
-    //  a, b, c all are elements of args array
-    //  ... is spread operator
-    //  more information here: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax
-
-    //  Our task is to determine what to return based on value and args
-    return null;
+  transform(value: number, ...args: string[]): string {
+    switch (args[0]) {
+      case 'INR':
+        //  Using JavaScript: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
+        //  +x will convert x to number
+        //  +args[1] || 2 = If args[1] is undefined (not supplied or non-number), use 2
+        return `₹ ${value.toFixed(+args[1] || 2)}`;
+      case 'USD':
+        return `$ ${value.toFixed(+args[1] || 2)}`;
+      default:
+        return 'Unknown currency';
+    }
   }
 }
