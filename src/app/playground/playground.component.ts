ebb10780905b03ef0078cff4d8a61ca7e72885b5
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-08 20:34:56 +0530

    done

--- src/app/playground/playground.component.ts
+++ src/app/playground/playground.component.ts
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./playground.component.css']
 })
 export class PlaygroundComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  handler() {
+    console.log('Button click handled');
   }
-
 }
