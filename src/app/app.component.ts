5eee115172409340595d951f731af207f032b2c7
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-08 21:05:51 +0530

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  //  Custom event binding
+  //  Step 2: Parent provides the handler for the custom event of the child
   title = 'ng-data-binding-demo';
+
+  parentHandler(args) {
+    console.log('Parent handler called', args);
+  }
 }
--- src/app/playground/playground.component.ts
+++ src/app/playground/playground.component.ts
@@ -6,22 +6,6 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./playground.component.css']
 })
 export class PlaygroundComponent implements OnInit {
-  //  Custom event-binding is how a child component can communicate with its parent
-
-  //  There are three steps to this communication
-
-  //  1.  Publish and expose the custom event in the child
-  //  2.  Parent hooks to this custom event by providing the handler
-  //  3.  At some juncture, the child emits the custom event
-
-  //  EventEmitter should come from @angular/core package
-  //  @Output() decorator exposes this custom event to the parent
-  //  Parent hooks (by providing the handler) to this event
-  //  in the same way a standard DOM event is hooked
-  //  It is possible to alias this customEvent field as @Output('alias')
-  //  This practice is discouraged and warned
-
-  //  Please note, this is step 1
   @Output() customEvent = new EventEmitter();
 
   constructor() {}
