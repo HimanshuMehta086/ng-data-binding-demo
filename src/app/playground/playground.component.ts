c84c33478ce1278460789a551d64ab7d061c303f
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-08 21:35:22 +0530

    comments added

--- src/app/playground/playground.component.ts
+++ src/app/playground/playground.component.ts
@@ -9,6 +9,10 @@ export class PlaygroundComponent implements OnInit {
   //  @Input() is a field-level decorator
   //  We normally do not initialize the @Input exposed fields
   //  We expect parent component to provide the value for these fields
+
+  //  It is possible to alias the title using @Input('alias')
+  //  This is discouraged and Angular runtime warns
+  //  Please refer to Angular Styleguide: https://angular.io/guide/styleguide#decorate-input-and-output-properties
   @Input() title: string;
 
   constructor() {}
