b5619c382796481490de99f74a8040ced6ffd924
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-08 21:19:09 +0530

    done

--- src/app/playground/playground.component.ts
+++ src/app/playground/playground.component.ts
@@ -8,14 +8,7 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 export class PlaygroundComponent implements OnInit {
   @Output() customEvent = new EventEmitter();
 
-  constructor() {
-    setTimeout(() => {
-      this.customEvent.emit({
-        on: new Date(),
-        message: 'Custom event emitted by a timer'
-      });
-    }, 5000);
-  }
+  constructor() {}
 
   ngOnInit() {}
 }
